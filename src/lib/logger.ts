5bd1617b183decf30cf0d27504919eb001652801
freefire448768676 <freefire4487686767@gmail.com>
2026-07-18 18:47:37 +0300

    Update logger.ts

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,4 @@
 export const logger = {
-  info: (msg: any) => console.log(``, msg),
-  error: (msg: any) => console.error(`[ERROR]`, msg),
-  warn: (msg: any) => console.warn(``, msg)
+  info: (msg: string) => console.log(`[INFO] ${msg}`),
+  error: (msg: any) => console.error(`[ERROR]`, msg)
 }
